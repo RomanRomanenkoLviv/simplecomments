21548799a95048f36cb9be14d65f30338323480c
Roman Zhyla <webmaster.doger@gmail.com>
2018-05-09 16:42:11 +0300

    first

---
+++ Plugin.php
@@ -0,0 +1,16 @@
+<?php namespace romanwebart\SimpleComments;
+
+use Backend;
+use System\Classes\PluginBase;
+
+class Plugin extends PluginBase
+{
+public function pluginDetails(){
+    return [
+        'name'    	=> 'Simple Comments',
+        'description' => 'Простая система комментариев',
+        'author'  	=> 'RomanWebart',
+        'icon'    	=> 'icon-bar-chart-o'
+    ];
+}
+} ?>
